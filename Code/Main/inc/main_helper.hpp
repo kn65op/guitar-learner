48722cc62df04eb51459d32e882c0cc724bd12c4
kn65op <kn65op@gmail.com>
2015-05-02 14:13:16 +0200

    Move helper functions to other file

---
+++ Code/Main/inc/main_helper.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+std::string getDBFileName();
+void readDB(const std::string& filename);
+
+
+
+
--- Code/Main/main.cpp
+++ Code/Main/main.cpp
@@ -4,58 +4,8 @@
 #include <cstdlib>
 #include "Main/inc/ProgramOptions.h"
 #include "inc/CommandFactory.h"
+#include "inc/main_helper.hpp"
 #include <TLogger.h>
-#include <OS/Factory.hpp>
-#include <Configuration/EqualsSignConfiguratorIO.hpp>
-
-std::string getDBFileName()
-{
-  THelper::OS::Factory osFactory;
-  auto osPaths = osFactory.getPaths();
-  const std::string default_config_file = osPaths->getHomeDir() + "/.guitar_learner/config.config";
-  const std::string default_database_name = osPaths->getHomeDir() + "/.guitar_learner/default.glearn";
-  const std::string db_file_param = "database";
-  THelper::Configuration::EqualsSignConfiguratorIO configuratorIO;
-  THelper::Configuration::Configurator conf;
-
-  LOG << "Opening config file: " << default_config_file;
-
-  std::ifstream in(default_config_file);
-  if (!in.is_open())
-  {
-    conf.storeStringParameter(db_file_param, default_database_name);
-    std::ofstream out(default_config_file);
-    configuratorIO.save(conf, out);
-    return default_database_name;
-  }
-  configuratorIO.load(conf, in);
-  try
-  {
-    return conf.loadStringParameter(db_file_param);
-  }
-  catch (THelper::Configuration::Configurator::NoSuchParameter&)
-  {
-    conf.storeStringParameter(db_file_param, default_database_name);
-    std::ofstream out(default_config_file);
-    configuratorIO.save(conf, out);
-    return default_database_name;
-  }
-}
-
-void readDB(const std::string& filename)
-{
-  LOG<< "Reading database from file: " << filename;
-  std::ifstream in(filename);
-  if (in.is_open())
-  {
-    Guitar::DatabaseFileReader::read(in);
-  }
-  else
-  {
-    std::cerr << "Unable to read database file, does not exists, will be created\n";
-    std::cout << "Creation of file: " << filename << "\n";
-  }
-}
 
 int main(int argc, const char *argv[])
 {
