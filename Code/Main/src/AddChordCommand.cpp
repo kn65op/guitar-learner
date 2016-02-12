a7d264865bac916441804052fa8873b63d58cd1f
kn65op <kn65op@gmail.com>
2016-02-12 22:40:16 +0100

    Fix #28

--- Code/Main/src/AddChordCommand.cpp
+++ Code/Main/src/AddChordCommand.cpp
@@ -13,6 +13,12 @@ void Main::AddChordCommand::process(const Main::GuitarLearnerCommand::CommandOpt
   std::cout << "State symbol for chord: ";
   Guitar::Chord::ChordNameType chord_name;
   std::cin >> chord_name;
+  if (Guitar::Chord::exists(chord_name))
+  {
+    LOG << "Chord exists: " << chord_name;
+    std::cout << "Chord already exists\n";
+    return;
+  }
   Guitar::Tab chord_tab = getTabFromInput(std::cout, std::cin);
   LOG << "Tab from frets created";
 
